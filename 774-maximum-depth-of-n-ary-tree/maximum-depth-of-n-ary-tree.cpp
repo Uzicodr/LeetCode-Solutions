40dbe37a31e7500ff67182ca1ad0e299b091ba67
UzairHaroon <168454395+Uzicodr@users.noreply.github.com>
2024-11-14 09:06:27 -0800

    Time: 15 ms (31.91%) | Memory: 15 MB (8.51%) - LeetSync

---
+++ 774-maximum-depth-of-n-ary-tree/maximum-depth-of-n-ary-tree.cpp
@@ -0,0 +1,40 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    int maxDepth(Node* root) {
+        if(root==NULL){
+            return 0;
+        }
+
+        vector<int> sizes(root->children.size());
+        
+        if (sizes.empty()) {
+            return 1;
+        }
+        for(int i=0;i<root->children.size();i++){
+            sizes[i]=maxDepth(root->children[i]);
+        }
+
+        sort(sizes.begin(),sizes.end());
+        return 1+sizes[sizes.size()-1];
+    }
+};
